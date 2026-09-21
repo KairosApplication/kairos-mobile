89fb5f3374a9464b85fc1313a038befb99e245e2
AnegueAlbq <enzo.anegue@germinare.org.br>
2026-09-21 08:50:25 -0300

    feat: add splash screen flow with 1st real screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-splashscreen:1.0.0")
     implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
